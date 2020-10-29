14d45eb8d9ea1f2d743f62f909a7702ea1b52064
ergunsahe <66415270+ergunsahe@users.noreply.github.com>
2020-10-29 22:25:22 +0100

    feat:add html and js files to convert a number in Roman Numeral

---
+++ index.js
@@ -0,0 +1,19 @@
+const userNumber = document.querySelector("#number");
+
+const result= document.querySelector("#display")
+
+const romanDict={"M":1000, "CM":900,"D":500, "CD":400, "C":100, "XC":90, "L":50,"XL":40,"X":10,"IX":9,"V":5,"IV":4,"I":1}
+function convertRomans(){
+    let romans=""
+    let number= parseInt(userNumber.value)
+    for (i in romanDict){
+        if(number >= romanDict[i]){
+            number -= romanDict[i]
+            romans += i 
+        }
+    }
+    result.innerText= `Roman Numeral of ${userNumber.value} is ${romans}.`
+
+}
+
+document.querySelector("#btn").addEventListener("click", convertRomans)
